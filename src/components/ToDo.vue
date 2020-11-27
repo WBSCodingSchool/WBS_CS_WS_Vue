641b847846382e64f2ea6c69cfaf68ab559118a8
Jorge Paul <jserranopaul@gmail.com>
2020-11-27 12:46:22 +0100

    Basic form

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,5 +1,13 @@
 <template>
   <h1 class="text-center">{{title}}</h1>
+  <form>
+  <div class="form-group">
+    <label for="to-do">What do you want to do?</label>
+    <input type="text" class="form-control" id="to-do" placeholder="Enter task">
+    <small id="emailHelp" class="form-text text-muted">You actually need to do it 🙃</small>
+  </div>
+  <button type="submit" class="btn btn-primary">Add</button>
+</form>
 </template>
 
 <script>
