201cd852dfa8bd9989c9c8fabd946243001b7fe8
Jorge Paul <jserranopaul@gmail.com>
2020-11-27 12:33:54 +0100

    Clean up + Bootstrap CSS

---
+++ src/components/ToDo.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-center">{{title}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
