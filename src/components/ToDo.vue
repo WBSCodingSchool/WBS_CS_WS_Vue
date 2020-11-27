b7bbeb3a4f9319002e0b44700e367ae5b0285f80
Jorge Paul <jserranopaul@gmail.com>
2020-11-27 13:46:33 +0100

    Component level state with ref and loggin user input

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -3,7 +3,7 @@
   <form  v-on:submit.prevent="addToDo">
   <div class="form-group">
     <label for="to-do">What do you want to do?</label>
-    <input type="text" class="form-control" id="to-do" placeholder="Enter task">
+    <input v-model="newToDo" type="text" class="form-control" id="to-do" placeholder="Enter task">
     <small id="emailHelp" class="form-text text-muted">You actually need to do it 🙃</small>
   </div>
   <button type="submit" class="btn btn-primary">Add</button>
@@ -11,16 +11,21 @@
 </template>
 
 <script>
+import {ref} from 'vue';
+
 export default {
   name: 'ToDo',
   props: {
     title: String
   },
   setup(){
+    const newToDo = ref('');
+
     const addToDo = () => {
-      console.log('Hello')
+      console.log(newToDo.value)
     }
     return{
+      newToDo,
       addToDo
     }
   }
